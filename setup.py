fdf3c4cc00d893d4d2f2ed8a7b8dbc712fbf2d3f
sandipan1998 <msandipan98@gmail.com>
2026-09-13 15:13:54 +0530

    files updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sandipan",
+    author_email="msandipan98@gmail.com",
+    packages=find_packages()
+)
